c4188427bcca032b944e49b7bdec66b608917993
Guillermo Miguel Cortez Riffel <guillermo.cortez@adventistas.org.ar>
2021-04-15 18:21:03 -0300

    agregue evnto en ok

--- getDataApi/alert.js
+++ getDataApi/alert.js
@@ -1,6 +1,9 @@
 
 const alerta = () =>{
-    swal("How Use! Speek the words", "  \n-Activate the page: Hello \n\n-Next section of cards: Next, following  \n\n-Previous card section: Previous, go back, back  \n\n-Redirect to repository containing project: Github, go to proyect   \n\n-To view credits: View credits, credits, creators");
+    swal("How Use! Speek the words", "  \n-Activate the page: Hello \n\n-Next section of cards: Next, following  \n\n-Previous card section: Previous, go back, back  \n\n-Redirect to repository containing project: Github, go to proyect   \n\n-To view credits: View credits, credits, creators")
+    .then((value)=>{
+        getData(API,home)
+    })
 }
 
 const credits = () =>{
