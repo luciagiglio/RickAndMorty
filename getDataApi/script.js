9b25d32b539da8a309f411a4725bf7f2fc5906ca
Guillermo Miguel Cortez Riffel <guillermo.cortez@adventistas.org.ar>
2021-04-15 15:47:46 -0300

    finally bitch

--- getDataApi/script.js
+++ getDataApi/script.js
@@ -2,12 +2,13 @@ var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
 var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
 var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
 
-var words = [ 'back', 'next', 'hello']; 
+var words = [ 'back', 'next', 'hello', 'git', 'github', 'previous', 'go back', 'following', 'home']; 
 var grammar = '#JSGF V1.0; grammar words; public <word> = ' + words.join(' | ') + ' ;'
 
 var recognition = new SpeechRecognition();
 var speechRecognitionList = new SpeechGrammarList();
 var i = 0
+var hello = true
 speechRecognitionList.addFromString(grammar, 1);
 recognition.grammars = speechRecognitionList;
 recognition.continuous = true;
@@ -18,33 +19,45 @@ recognition.maxAlternatives = 1;
 
 
 recognition.onresult = function(event) {
-  // The SpeechRecognitionEvent results property returns a SpeechRecognitionResultList object
-  // The SpeechRecognitionResultList object contains SpeechRecognitionResult objects.
-  // It has a getter so it can be accessed like an array
-  // The first [0] returns the SpeechRecognitionResult at the last position.
-  // Each SpeechRecognitionResult object contains SpeechRecognitionAlternative objects that contain individual results.
-  // These also have getters so they can be accessed like arrays.
-  // The second [0] returns the SpeechRecognitionAlternative at position 0.
-  // We then return the transcript property of the SpeechRecognitionAlternative object
-  //var color = event.results[i][0].transcript;
-  //diagnostic.textContent = 'Result received: ' + color + '.';
-  //bg.style.backgroundColor = color;
-  //console.log('Result: ' + event.results[i][0].transcript);
+
   let str = event.results[i][0].transcript
+  console.log(str)
   switch(str.toLowerCase().trim())
   {
     case "hello":
       getData(API,home)
+      if(hello)
+      {
+        miAudio.play()
+        hello = false
+      }
       break
     case "next":
+      getData(next)
+      break
+    case "following":
       getData(next)
       break
     case "back":
       getData(preview)
       break
-    case "":
+    case "go back":
+      getData(preview)
       break
-    case "":
+    case "previous":
+      getData(preview)
+      break
+    case "show github":
+      redirGit()
+      break
+    case "github":
+      redirGit()
+      break
+    case "open project":
+      redirGit()
+      break
+    case "go to project":
+      redirGit()
       break
     case "":
       break
@@ -74,5 +87,11 @@ recognition.onspeechend = function() {
   recognition.stop();
 }
 
-recognition.start();
+document.body.onclick = function() {
+  recognition.start();
+}
 
+const redirGit = () =>
+{
+  window.open('https://github.com/luciagiglio/RickAndMorty', '_blank');
+}
